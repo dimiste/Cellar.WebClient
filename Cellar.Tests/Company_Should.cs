8d30f5643116bdef4d4a1fe9e023285b8a6bbd60
Dimitar Stefanov Stefanov <dimiste@yahoo.com>
2018-10-30 21:43:07 +0100

    AnyTests

--- Cellar.Tests/Company_Should.cs
+++ Cellar.Tests/Company_Should.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Cellar.Data.Models;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,31 @@ namespace Cellar.Tests
     [TestFixture]
     public class Company_Should
     {
+        [Test]
+        public void ReturnValuesPropertiesCorrect_WhenSetPropertiesAreCorrect()
+        {
+            // Arrange
+            var company = new Company();
+            var name = "Name";
+            var number = "123";
+            var commercial = "Commercial";
 
+            // Act
+            company.Id = 1;
+            company.Name = name;
+            company.Number = number;
+            company.Commercial = commercial;
+
+            var expectedId = 1;
+            var expectedName = name;
+            var expectedNumber = number;
+            var expectedCommercial = commercial;
+
+            // Assert
+            Assert.AreEqual(expectedId, company.Id);
+            Assert.AreEqual(expectedName, company.Name);
+            Assert.AreEqual(expectedNumber, company.Number);
+            Assert.AreEqual(expectedCommercial, company.Commercial);
+        }
     }
 }
---
+++ Cellar.Tests/SumBills_Should.cs
@@ -0,0 +1,69 @@
+﻿using Cellar.Data.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cellar.Tests
+{
+    [TestFixture]
+    public class SumBills_Should
+    {
+        
+        [Test]
+        public void ReturnValuesPropertiesCorrect_WhenSetPropertiesAreCorrect()
+        {
+            var sumBills = new SumBills();
+            var mes = "01";
+            var exento = 1.0;
+            var baseSuperReducido = 2.0;
+            var baseReducido = 3.0;
+            var baseNormal = 4.0;
+            var ivaSuperRedudico = 5.0;
+            var ivaReducido = 6.0;
+            var ivaNormal = 7.0;
+            var baseTotal = 8.0;
+            var ivaTotal = 9.0;
+            var importeTotal = 10.0;
+
+
+            sumBills.Mes = mes;
+            sumBills.Exento = exento;
+            sumBills.BaseSuperReducido = baseSuperReducido;
+            sumBills.BaseReducido = baseReducido;
+            sumBills.BaseNormal = baseNormal;
+            sumBills.IVASuperReducido = ivaSuperRedudico;
+            sumBills.IVAReducido = ivaReducido;
+            sumBills.IVANormal = ivaNormal;
+            sumBills.BaseTotal = baseTotal;
+            sumBills.IVATotal = ivaTotal;
+            sumBills.ImporteTotal = importeTotal;
+
+            var expectedMes = mes;
+            var expectedExento = exento;
+            var expectedSuperReducido = baseSuperReducido;
+            var expectedReducido = baseReducido;
+            var expectedNormal = baseNormal;
+            var expectedIvaSuperReducido = ivaSuperRedudico;
+            var expectedIvaReducido = ivaReducido;
+            var expectedIvaNormal = ivaNormal;
+            var expectedBaseTotal = baseTotal;
+            var expectedIvaTotal = ivaTotal;
+            var expectedImporteTotal = importeTotal;
+
+            Assert.AreEqual(expectedMes, sumBills.Mes);
+            Assert.AreEqual(expectedExento, sumBills.Exento);
+            Assert.AreEqual(expectedSuperReducido, baseSuperReducido);
+            Assert.AreEqual(expectedReducido, sumBills.BaseReducido);
+            Assert.AreEqual(expectedNormal, sumBills.BaseNormal);
+            Assert.AreEqual(expectedIvaSuperReducido, sumBills.IVASuperReducido);
+            Assert.AreEqual(expectedIvaReducido, sumBills.IVAReducido);
+            Assert.AreEqual(expectedIvaNormal, sumBills.IVANormal);
+            Assert.AreEqual(expectedBaseTotal, sumBills.BaseTotal);
+            Assert.AreEqual(expectedIvaTotal, sumBills.IVATotal);
+            Assert.AreEqual(expectedImporteTotal, sumBills.ImporteTotal);
+        }
+    }
+}
